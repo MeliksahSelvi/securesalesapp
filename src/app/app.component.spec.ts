b82da5a9dabdad70d22aaff8a5a74bfd0c3181b8
mselvi <mselvi@uni-yaz.com>
2023-08-30 17:02:41 +0300

    revised project configuration

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'securesalesapp'`, () => {
+  it(`should have as title 'securesalesangular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('securesalesapp');
+    expect(app.title).toEqual('securesalesangular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('securesalesapp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('securesalesangular app is running!');
   });
 });
