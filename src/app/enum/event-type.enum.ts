81d292f041be46592fcedd7c94587d65838d22f5
mselvi <mselvi@uni-yaz.com>
2023-09-11 13:15:26 +0300

    added roletype,event-type,datastate enum

---
+++ src/app/enum/event-type.enum.ts
@@ -0,0 +1,11 @@
+export enum EventType {
+  LOGIN_ATTEMPT = "Login Attempt",
+  LOGIN_ATTEMPT_FAILURE = "Login Attempt Failure",
+  LOGIN_ATTEMPT_SUCCESS = "Login Attempt Success",
+  PROFILE_UPDATE = "Profile Update",
+  PROFILE_PICTURE_UPDATE = "Profile Picture Update",
+  ROLE_UPDATE = "Role Update",
+  ACCOUNT_SETTINGS_UPDATE = "Account Settings Update",
+  PASSWORD_UPDATE = "Password Update",
+  MFA_UPDATE = "MFA Update"
+}
---
+++ src/app/enum/roletype.enum.ts
@@ -0,0 +1,7 @@
+export enum RoleType {
+
+  ROLE_USER = "Role User",
+  ROLE_MANAGER = "Role Manager",
+  ROLE_ADMIN = "Role Admin",
+  ROLE_SYSADMIN = "Role SysAdmin"
+}
