c837751a7a54001cbf24360a34cdc787ff7afde4
mselvi <mselvi@uni-yaz.com>
2023-09-15 23:26:41 +0300

    -revised main app module and component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./component/home/home/home.component";
+import {AuthenticationGuard} from "./component/guard/authentication.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'profile', loadChildren: () => import('./component/profile/user.module').then(module => module.UserModule)},
+  {path: '', redirectTo: '/', pathMatch: 'full'},
+  {path: '**', component: HomeComponent, canActivate: [AuthenticationGuard]}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CoreModule} from "./core/core.module";
+import {AuthModule} from "./component/auth/auth.module";
+import {HomeModule} from "./component/home/home.module";
+import {InvoiceModule} from "./component/invoice/invoice.module";
+import {CustomerModule} from "./component/customer/customer.module";
+import {NotificationModule} from "./notification.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CoreModule,
+    AuthModule,
+    CustomerModule,
+    InvoiceModule,
+    HomeModule,
+    AppRoutingModule,
+    NotificationModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
